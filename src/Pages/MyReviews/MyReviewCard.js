16a91d63622de6c41fc626ab3e10a2c2d72cfc17
arafatakhter505 <arafatakhterforhad@gmail.com>
2022-11-08 20:38:23 +0600

    modal add

--- src/Pages/MyReviews/MyReviewCard.js
+++ src/Pages/MyReviews/MyReviewCard.js
@@ -1,21 +1,64 @@
 import React from "react";
 import { SlNote, SlTrash } from "react-icons/sl";
 
-const MyReviewCard = ({ review }) => {
+const MyReviewCard = ({ review, handleReviewDelete, handleReviewEdit }) => {
   return (
     <div className="card bg-base-100 shadow-xl border">
       <div className="card-body">
-        <h2 className="card-title">{review.name}</h2>
+        <h2 className="card-title">{review.serviceName}</h2>
         <p>{review.review}</p>
         <div className="card-actions justify-end">
-          <button className="badge badge-outline">
+          {/* The button to open modal */}
+          <label
+            htmlFor={`my-modal-${review._id}`}
+            className="badge badge-outline cursor-pointer"
+          >
             <SlNote />
             Edit
-          </button>
-          <button className="badge badge-outline">
+          </label>
+          <button
+            onClick={() => handleReviewDelete(review._id)}
+            className="badge badge-outline"
+          >
             <SlTrash />
             Delete
           </button>
+
+          {/* Put this part before </body> tag */}
+          <input
+            type="checkbox"
+            id={`my-modal-${review._id}`}
+            className="modal-toggle"
+          />
+          <div className="modal">
+            <div className="modal-box relative">
+              <label
+                htmlFor={`my-modal-${review._id}`}
+                className="btn btn-sm btn-circle absolute right-2 top-2"
+              >
+                ✕
+              </label>
+              <h3 className="text-lg font-bold mb-3">
+                Edit {review.serviceName} Service Review
+              </h3>
+              <form onSubmit={handleReviewEdit}>
+                <textarea
+                  className="textarea w-full textarea-bordered h-[150px] py-4 mb-3"
+                  placeholder="reviews"
+                  name="review"
+                  defaultValue={review.review}
+                ></textarea>
+                <div className="modal-action">
+                  <button
+                    type="submit"
+                    className="bg-blue-600 text-white px-6 py-2 rounded"
+                  >
+                    Update
+                  </button>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/Pages/MyReviews/MyReviews.js
+++ src/Pages/MyReviews/MyReviews.js
@@ -1,10 +1,30 @@
 import React, { useContext, useEffect, useState } from "react";
 import { AuthContext } from "../../contexts/UserContext";
 import MyReviewCard from "./MyReviewCard";
+import { toast } from "react-hot-toast";
 
 const MyReviews = () => {
   const { user } = useContext(AuthContext);
   const [reviews, setReviews] = useState([]);
+
+  const handleReviewDelete = (id) => {
+    fetch(`http://localhost:5000/reviews/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          const remaining = reviews.filter((review) => review._id !== id);
+          setReviews(remaining);
+          toast.success("Successfully delete");
+        }
+      });
+  };
+
+  const handleReviewEdit = (event) => {
+    event.preventDefault();
+    console.log(event.target.review.value);
+  };
 
   useEffect(() => {
     fetch(`http://localhost:5000/reviews?email=${user?.email}`)
@@ -13,11 +33,16 @@ const MyReviews = () => {
   }, [user?.email]);
 
   return (
-    <div className="lg:px-20 md:px-8 px-6 min-h-[200px] my-12">
+    <div className="lg:px-20 md:px-8 px-6 md:min-h-[200px] min-h-screen my-12">
       {reviews.length > 0 ? (
         <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-10">
           {reviews.map((review) => (
-            <MyReviewCard key={review._id} review={review} />
+            <MyReviewCard
+              key={review._id}
+              review={review}
+              handleReviewDelete={handleReviewDelete}
+              handleReviewEdit={handleReviewEdit}
+            />
           ))}
         </div>
       ) : (
